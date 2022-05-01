c9cf1a33ea84e6d8130ec6161cd916cd213f817c
alex <alexrafael10@gmail.com>
2022-04-30 22:34:51 -0300

    complete solution

--- k-distant-indexes/index.ts
+++ k-distant-indexes/index.ts
@@ -1,14 +1,23 @@
 export const findKDistantIndices = (nums: number[], key: number, k: number): number[] => {
-    const secondConditionIndexes = nums.map((num, index) => num === key ? index : null).filter(num => !!num);
+    const secondConditionIndexes = nums.map((num, index) =>(num === key ? index : null)).filter(num => num !== null);
 
-    return nums
+    console.log({secondConditionIndexes});
+    
+
+    const result = nums
       .reduce((acc, _, i) => {
         const absoluteLessThanK = secondConditionIndexes.some(
-          (j) => Math.abs(i - j) <= k
+          j => Math.abs(i - j) <= k
         );
 
-        if (absoluteLessThanK) acc.push(i);
+        if (absoluteLessThanK) {
+          acc.push(i);          
+        }
 
         return acc;
       }, []);
+
+    console.log({result});
+
+    return result;
 };
