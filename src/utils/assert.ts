e8630931f092f4cac7d68edf9bf0724543bc1105
Alexis R <alexrafael10@gmail.com>
2022-06-27 18:30:51 -0300

    updated mini assert lib

--- src/utils/assert.ts
+++ src/utils/assert.ts
@@ -1,34 +1,38 @@
 import { isEquals } from "../array/index.js";
 import { Logger } from "../utils.js";
 
-type AssertionCallback = (expected: unknown) => boolean;
+type AssertionCallback = (...params: unknown[]) => boolean;
 
 const TAB = "  ";
 const SUCCESS_MESSAGE = "✓ Assertion Passed";
-const FAIL_MESSAGE = "✘ Assertion Failed"
+const FAIL_MESSAGE = "✘ Assertion Failed";
+
+const logger = new Logger();
 
 const logResults = (assertion: boolean, expected, received) => {
   if (assertion) {
-    Logger.info(SUCCESS_MESSAGE);
+    logger.info(SUCCESS_MESSAGE);
     return;
   }
 
-  const errorMessage = `${TAB}- Expected: ${expected}\n${TAB}+ Received: ${received}`
-  Logger.error(FAIL_MESSAGE);
-  Logger.info(errorMessage);
+  const errorMessage = `${TAB}- Expected: ${expected}\n${TAB}+ Received: ${received}`;
+  logger.error(FAIL_MESSAGE);
+  logger.info(errorMessage);
 };
 
-const gatherResults = (received, assertionCb: AssertionCallback) => {
-  return (expected) => {
-    const assertion = assertionCb?.(expected);
-    logResults(assertion, expected, received);
-  }
-}
+const runComparator = (
+  comparator: AssertionCallback,
+  received: unknown,
+  expected?: unknown
+) => {
+  const params = [received];
+  if (expected) params.push(expected);
+  logResults(comparator(...params), expected, received);
+};
 
-export const assert = (received: unknown) => {
-  const eq = (expected: unknown) => isEquals(expected, received);
-
+export const expect = (received: unknown) => {
   return {
-    eq: gatherResults(received, eq)
-  }
+    toBe: (expected: unknown) => runComparator(isEquals, received, expected),
+    toBeFalsy: () => runComparator(() => !!received, received, expect),
+  };
 };
