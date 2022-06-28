b555523e2831f2aa5dafd07a976f1ec07cf71f96
Alexis R <alexrafael10@gmail.com>
2022-06-28 12:55:01 -0300

    string > is plaindrome

--- src/string/index.ts
+++ src/string/index.ts
@@ -1,3 +1,5 @@
+import Logger from "../utils/Logger.js";
+
 export const longestPalindrome = (s: string): string => {
   return s[0];
 
@@ -13,4 +15,32 @@ export const longestPalindrome = (s: string): string => {
       //
     }
   }
+};
+
+export const isPalindrome = (s: string): boolean => {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left <= right) {
+    const charLeft = s.charAt(left).toLocaleLowerCase();
+    if (!/[a-z0-9]/.test(charLeft)) {
+      left++;
+      continue;
+    }
+
+    const charRight = s.charAt(right).toLocaleLowerCase();
+    if (!/[a-z0-9]/.test(charRight)) {
+      right--;
+      continue;
+    }
+
+    if (charLeft !== charRight) {
+      return false;
+    }
+
+    left++;
+    right--;
+  }
+
+  return true;
 };
